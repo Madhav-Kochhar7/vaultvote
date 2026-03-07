89a9953acb71b1fdb74875bc06bc875d7794b4af
Madhav Kochhar <kochhar.madhavv@gmail.com>
2026-03-07 22:02:57 +0530

    ‎ ‎ ‎

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
 // postcss.config.js
-=======
->>>>>>> a2bc7a24f18339832d3e562ddeca823ef20d8f41
 export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
-<<<<<<< HEAD
 };
-=======
-}
->>>>>>> a2bc7a24f18339832d3e562ddeca823ef20d8f41
